58df2004e47e7d2d4fe4040327b4afe8b27c8571
James Strobel <strobeljames20@gmail.com>
2025-01-27 15:27:48 -0700

    Homework-Push

---
+++ Sub-Projects/JS-Debugging/debugging.js
@@ -0,0 +1,14 @@
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+    return radius * radius * PI;
+}
+
+area = circleArea(3);
+console.log(area);
+
+area = circleArea(5);
+console.log(area);
+
+area = circleArea(8);
+console.log(area);
