0fe6d5881510b874b49df83ef1f3601205c32205
James Strobel <strobeljames20@gmail.com>
2026-01-10 20:16:27 -0700

    WDD 131 - W1

--- Sub-Projects/JS-Debugging/debugging.js
+++ Sub-Projects/JS-Debugging/debugging.js
@@ -12,3 +12,15 @@ console.log(area);
 
 area = circleArea(8);
 console.log(area);
+
+
+// Scope Example
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
