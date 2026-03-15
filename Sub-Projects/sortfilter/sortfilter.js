e5a9be6135135a62ae79eaa801093003ed5e8c8c
James Strobel <strobeljames20@gmail.com>
2026-03-14 20:34:48 -0600

    hikes

---
+++ Sub-Projects/sortfilter/sortfilter.js
@@ -0,0 +1,35 @@
+nums = [12, 10, 8, 3]
+
+console.log(nums.sort(compareFn));
+
+
+function compareFn(a,b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+ return 0;
+}
+                
+
+const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
+                
+let simpleSort = simpleList.sort();
+
+console.log(simpleSort);
+
+let lowerList = simpleList.map(function(fruit){ 
+    return fruit.toLowerCase();
+})
+
+let lowerSort = lowerList.sort();
+console.log(lowerSort);
+
+let searchTerm = 'an';
+
+let filterFruit = lowerList.filter(searchFruit);
+
+function searchFruit(fruit) {
+    return fruit.includes(searchTerm);
+}
